5ba839882d09ac07ee2b80ca83b53b030f4a03b0
jodhyyy <jodhyh71@gmail.com>
2023-10-19 00:40:44 +0700

    update

--- app/src/main/java/com/example/macro/AlamatPickupActivity.kt
+++ app/src/main/java/com/example/macro/AlamatPickupActivity.kt
@@ -1,11 +1,26 @@
 package com.example.macro
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.macro.databinding.ActivityAlamatPickupBinding
 
 class AlamatPickupActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityAlamatPickupBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_alamat_pickup)
+        binding = ActivityAlamatPickupBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnPickupSelanjutnya.setOnClickListener {
+            startActivity(Intent(this, PickUpBerhasilActivity::class.java))
+        }
+        binding.ibHomePickup.setOnClickListener {
+            startActivity(Intent(this, DashboardActivity::class.java))
+        }
+        binding.rlPilihPetaPickup.setOnClickListener {
+            startActivity(Intent(this, MapsActivity::class.java))
+        }
     }
 }
--- app/src/main/java/com/example/macro/MapsActivity.kt
+++ app/src/main/java/com/example/macro/MapsActivity.kt
@@ -1,11 +1,23 @@
 package com.example.macro
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.macro.databinding.ActivityMapsBinding
 
 class MapsActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMapsBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_maps)
+        binding = ActivityMapsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnKembali.setOnClickListener {
+            startActivity(Intent(this, AlamatPickupActivity::class.java))
+        }
+        binding.btnUbah.setOnClickListener {
+            startActivity(Intent(this, AlamatPickupActivity::class.java))
+        }
     }
 }

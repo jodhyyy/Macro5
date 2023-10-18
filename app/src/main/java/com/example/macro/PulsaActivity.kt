20006117fdfde28d796dcf180ed23653a6ec54fe
jodhyyy <jodhyh71@gmail.com>
2023-10-18 21:55:15 +0700

    update

--- app/src/main/java/com/example/macro/PulsaActivity.kt
+++ app/src/main/java/com/example/macro/PulsaActivity.kt
@@ -1,11 +1,23 @@
 package com.example.macro
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.macro.databinding.ActivityPulsaBinding
 
 class PulsaActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityPulsaBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_pulsa)
+        binding = ActivityPulsaBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.ibHomePulsa.setOnClickListener {
+            startActivity(Intent(this, DashboardActivity::class.java))
+        }
+        binding.btnBankSelanjutnya.setOnClickListener {
+            startActivity(Intent(this, ValidasiPulsaActivity::class.java))
+        }
     }
 }
--- app/src/main/java/com/example/macro/ValidasiPulsaActivity.kt
+++ app/src/main/java/com/example/macro/ValidasiPulsaActivity.kt
@@ -1,11 +1,23 @@
 package com.example.macro
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.macro.databinding.ActivityValidasiPulsaBinding
 
 class ValidasiPulsaActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityValidasiPulsaBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_validasi_pulsa)
+        binding = ActivityValidasiPulsaBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.ibHomeValidasiPulsa.setOnClickListener {
+            startActivity(Intent(this, DashboardActivity::class.java))
+        }
+        binding.ibBackArrowValidasiPulsa.setOnClickListener {
+            startActivity(Intent(this, PulsaActivity::class.java))
+        }
     }
 }

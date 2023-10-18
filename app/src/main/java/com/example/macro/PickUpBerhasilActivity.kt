ca5c262c5cfb586073cb1e9b26cff1b027d6ec0c
Rizky Zakaria Rani <klupa693@gmail.com>
2023-10-18 20:51:12 +0700

    Frame Pulsa

--- app/src/main/java/com/example/macro/PickUpBerhasilActivity.kt
+++ app/src/main/java/com/example/macro/PickUpBerhasilActivity.kt
@@ -3,9 +3,9 @@ package com.example.macro
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class PickUpActivity : AppCompatActivity() {
+class PickUpBerhasilActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_pick_up)
+        setContentView(R.layout.activity_pick_up_berhasil)
     }
 }

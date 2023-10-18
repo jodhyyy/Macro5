955d6f4b15169373b38f2ba6eedbc346a2b6ddfb
malikaziz <azizak2119@gmail.com>
2023-10-18 20:26:56 +0800

    fix layout bank dan validasi

---
+++ app/src/main/java/com/example/macro/EWaletActivity.kt
@@ -0,0 +1,11 @@
+package com.example.macro
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EWaletActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ewalet)
+    }
+}
